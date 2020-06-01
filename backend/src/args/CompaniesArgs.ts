5e035c82b41601682888cb6a62e7d671775a91a2
Ali Muzaffar <ali@sonderaustralia.com>
2020-06-01 11:22:38 +1000

    Better handling of arguments for companies

---
+++ backend/src/args/CompaniesArgs.ts
@@ -0,0 +1,75 @@
+enum SortBy {
+    volatile = 'volatile',
+    score = 'score'
+}
+
+enum SortDirection {
+    asc = 'asc',
+    desc = 'desc'
+}
+
+enum FilterField {
+    total = 'total',
+    exchange_symbol = 'exchange_symbol'
+}
+
+export interface IArgs {
+    validate(): void
+}
+
+export class CompaniesArgs implements IArgs {
+    sortBy: SortBy
+    sortDirection: SortDirection
+    filterByField: FilterField
+    filterByValues: string[]
+
+    constructor({sortBy, sortDirection, filterByField, filterByValues}: CompaniesArgs) {
+        this.sortBy = sortBy
+        this.sortDirection = sortDirection
+        this.filterByField = filterByField
+        this.filterByValues = filterByValues
+    }
+
+    validate(): void {
+        let errorMessage = ''
+
+        if (stringDefined(this.sortBy) || stringDefined(this.sortDirection)) {
+            errorMessage += this.validateSort()
+        }
+
+        if (stringDefined(this.filterByField) || this.filterByValues) {
+            errorMessage += this.validateFilter()
+        }
+
+        if (errorMessage.length > 0) {
+            throw new Error(errorMessage)
+        }
+    }
+
+    validateSort(): string {
+        if (!Object.values(SortBy).includes(this.sortBy)) {
+            return `Valid values for sortBy are: ${Object.values(SortBy)}\n`
+        }
+        if (!Object.values(SortDirection).includes(this.sortDirection)) {
+            return `Valid values for sortDirection are: ${Object.values(SortDirection)}\n`
+        }
+        return ''
+    }
+
+    validateFilter(): string {
+        if (!Object.values(FilterField).includes(this.filterByField)) {
+            return `Valid values for filterByField are: ${Object.values(FilterField)}\n`
+        }
+
+        if (!this.filterByValues || this.filterByValues.length === 0) {
+            return `filterByValues cannot be empty\n`
+        }
+
+        return ''
+
+    }
+}
+
+function stringDefined(str: string) {
+    return (str !== undefined && str !== null)
+}
--- backend/src/resolvers.ts
+++ backend/src/resolvers.ts
@@ -1,4 +1,4 @@
-import { strIsEmpty, strIsNotEmpty } from './utils'
+import {CompaniesArgs} from "./args/CompaniesArgs"
 
 export default {
   Query: {
@@ -10,39 +10,8 @@ export default {
     },
     companies: (_source: any, args: any, context: any) => {
       const { sortBy, sortDirection, filterByField, filterByValues } = args
-      if (strIsNotEmpty(sortBy) && !['volatile', 'score'].includes(sortBy)) {
-        throw new Error(`Valid values for sortBy are ${['volatile', 'score']}`)
-      }
-      if (
-        strIsNotEmpty(sortDirection) &&
-        (strIsEmpty(sortBy) || !['desc', 'asc'].includes(sortDirection))
-      ) {
-        throw new Error(
-          `Valid values for sortDirection are ${[
-            'desc',
-            'asc',
-          ]} and sortBy must be defined`
-        )
-      }
-      if (
-        strIsNotEmpty(filterByField) &&
-        !['total', 'exchange_symbol'].includes(filterByField)
-      ) {
-        throw new Error(
-          `Valid values for filterByField are ${[
-            'total',
-            'exchange_symbol',
-          ]} and filterByField must be defined`
-        )
-      }
-      if (
-        filterByValues &&
-        (strIsEmpty(filterByField) || filterByValues.length < 1)
-      ) {
-        throw new Error(
-          'filterByField must be defined and filterByValues must contain at least 1 element in an array'
-        )
-      }
+      const companyArgs = new CompaniesArgs(args)
+      companyArgs.validate()
       return context.dataSources.db.getCompaniesSortedAndFiltered(
         sortBy,
         sortDirection,
